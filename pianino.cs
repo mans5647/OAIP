c35f2ae38dba392d8df67660b13d0cd2f6e4410f
Mans <mansurgalimov161@gmail.com>
2022-10-13 19:29:21 +0300

    Added pianino on OAIP 14.10.22

---
+++ pianino.cs
@@ -0,0 +1,195 @@
+using System;
+
+class SS
+{
+static void usage()
+{
+	string menu = "Choosing from octaves: \n"+
+				  "Tab - 1 octave\n"+
+				  "F2  - 2 octave\n"+
+				  "F3  - 3 octave\n"+
+				  "Esc - to exit. Available in octaves as well\n";
+	Console.WriteLine(menu);
+}
+static void RegSound(int c)
+{
+	Console.Beep(c, 255);
+}
+static void qwerty(int [] patt)
+{
+	
+	ConsoleKeyInfo symbol;
+	Console.Write("Welcome to: `Octave 1' ! Hit to play >>> ");
+	do
+	{
+		symbol = Console.ReadKey();
+		if (symbol.Key == ConsoleKey.Q)
+		{
+			int vice = patt[0];
+			RegSound(vice);
+		}
+		else if (symbol.Key == ConsoleKey.W)
+		{
+			int vice = patt[1];
+			RegSound(vice);
+		}
+		else if (symbol.Key == ConsoleKey.E)
+		{
+			int vice = patt[2];
+			RegSound(vice);
+		}
+		else if (symbol.Key == ConsoleKey.R)
+		{
+			int vice = patt[3];
+			RegSound(vice);
+		}
+		else if (symbol.Key == ConsoleKey.T)
+		{
+			int vice = patt[4];
+			RegSound(vice); 
+		}
+		else if (symbol.Key == ConsoleKey.Y)
+		{
+			int vice = patt[5];
+			RegSound(vice);
+		}
+		else if (symbol.Key == ConsoleKey.U)
+		{
+			int vice = patt[6];
+			RegSound(vice);
+		}
+		else if (symbol.Key == ConsoleKey.I)
+		{
+			int vice = patt[7];
+			RegSound(vice);
+		}
+	} while (symbol.Key != ConsoleKey.Escape);
+	Console.WriteLine("\n\nQuitting from Octave 1 !\n");
+}
+static void lower_Q(int [] patt2)
+{
+	ConsoleKeyInfo sym;
+	Console.Write("Welcome to: `Octave 2' ! Hit to play >>> ");
+	do
+	{
+		sym = Console.ReadKey();
+		if (sym.Key == ConsoleKey.A)
+		{
+			int vice = patt2[0];
+			RegSound(vice);
+		}
+		else if (sym.Key == ConsoleKey.S)
+		{
+			int vice = patt2[1];
+			RegSound(vice);
+		}
+		else if (sym.Key == ConsoleKey.D)
+		{
+			int vice = patt2[2];
+			RegSound(vice);
+		}
+		else if (sym.Key == ConsoleKey.F)
+		{
+			int vice = patt2[3];
+			RegSound(vice);
+		}
+		else if (sym.Key == ConsoleKey.G)
+		{
+			int vice = patt2[4];
+			RegSound(vice); 
+		}
+		else if (sym.Key == ConsoleKey.H)
+		{
+			int vice = patt2[5];
+			RegSound(vice);
+		}
+		else if (sym.Key == ConsoleKey.J)
+		{
+			int vice = patt2[6];
+			RegSound(vice);
+		}
+		else if (sym.Key == ConsoleKey.K)
+		{
+			int vice = patt2[7];
+			RegSound(vice);
+		}  
+	} while (sym.Key != ConsoleKey.Escape);
+	Console.WriteLine("\n\nQuitting from octave 2 !\n");
+}
+static void zxc(int [] patt3)
+{
+	ConsoleKeyInfo sym;
+	Console.Write("Welcome to: `Octave 3' ! Hit to play >>> ");
+	do
+	{
+		sym = Console.ReadKey();
+		if (sym.Key == ConsoleKey.Z)
+		{
+			int vice = patt3[0];
+			RegSound(vice);
+		}
+		else if (sym.Key == ConsoleKey.X)
+		{
+			int vice = patt3[1];
+			RegSound(vice);
+		}
+		else if (sym.Key == ConsoleKey.C)
+		{
+			int vice = patt3[2];
+			RegSound(vice);
+		}
+		else if (sym.Key == ConsoleKey.V)
+		{
+			int vice = patt3[3];
+			RegSound(vice);
+		}
+		else if (sym.Key == ConsoleKey.B)
+		{
+			int vice = patt3[4];
+			RegSound(vice); 
+		}
+		else if (sym.Key == ConsoleKey.N)
+		{
+			int vice = patt3[5];
+			RegSound(vice);
+		}
+		else if (sym.Key == ConsoleKey.M)
+		{
+			int vice = patt3[6];
+			RegSound(vice);
+		}
+	} while (sym.Key != ConsoleKey.Escape);
+	Console.WriteLine("\n\nQuitting from octave 3 !\n");
+}
+static void Main() 
+{
+	
+	int [] oct1 = new int[] {659, 462, 534, 699, 744, 233, 345, 543}; 	
+	int [] oct2 = new int[] {341, 455, 266, 560, 302, 231, 444, 312};
+	int [] oct3 = new int[] {300, 400, 500, 362, 564, 313, 631, 189};
+	ConsoleKeyInfo main_key;
+
+	
+	do
+	{
+		usage();
+			main_key = Console.ReadKey();
+			if (main_key.Key == ConsoleKey.Tab)
+			{
+				Console.WriteLine("Oct: 1 | Avail. keys : q , w , e , r , t , y , u , i");
+				qwerty(oct1);
+			}
+			else if(main_key.Key == ConsoleKey.F2)
+			{
+				Console.WriteLine("Oct: 2 | Avail. keys : a , s , d , f , g , h , j , k");
+				lower_Q(oct2);
+			}
+			else if (main_key.Key == ConsoleKey.F3)
+			{
+				Console.WriteLine("Oct: 3 | Avail. keys : z , x , c , v , b , n , m");
+				zxc(oct3);
+			}
+	} while (main_key.Key != ConsoleKey.Escape);
+	
+}
+}
