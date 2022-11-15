307179f29bfa5172091225c0f80affab09befdda
mans5647 <mansurgalimov161@gmail.com>
2022-11-15 21:56:47 +0300

    Added Converter 6th pr work

---
+++ converter/Converterr/convert_properties.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    public class Company_model
+    {
+        public string CompanyName = string.Empty;
+        public string CompanyHeadquarter = string.Empty;
+        public int CompanyNumOFEmployees;
+        public long CompanyRevenue;
+
+        public Company_model(string name, string hq, int nofe, long revenue)
+        {
+            CompanyName = name;
+            CompanyHeadquarter = hq;
+            CompanyNumOFEmployees = nofe;
+            CompanyRevenue = revenue;
+        }
+    }
+}
