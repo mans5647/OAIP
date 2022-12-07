2f6a4897e42e55d0724eec32b0682cefaaad5548
Galimov Mansur <mansurgalimov08@gmail.com>
2022-12-07 08:54:02 +0300

    Added: Fasttyper!

---
+++ FAST_TYPER_8/FastTyper/Class1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+namespace FastTyper
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public uint SymsPerMins { get; set; }
+        public uint SymsPerSecs { get; set; }
+    }
+
+    public class Table
+    {
+        bool FillTable(string name, uint symbols_per_min, uint symbols_per_sec)
+        {
+            bool already_exists = false;
+            Console.WriteLine("Table: \n");
+            List<object> records = new List<object>();
+            if ((already_exists = records.Contains(name)) == true || (already_exists = records.Contains(symbols_per_min)) == true || (already_exists = records.Contains(symbols_per_sec)) == true)
+            {
+                return false;
+            }
+            else
+            {
+                records.Add(name);
+                records.Add(symbols_per_min);
+                records.Add(symbols_per_sec);
+            }
+                foreach (var objetcs in records)
+            {
+                Console.WriteLine(name+"\t"+symbols_per_min+"\t"+symbols_per_sec);
+                
+            }
+            return true;
+        }
+    }
+    public class Enter
+    {
+        public static void TextChallenge(string player_name, uint symbols_per_min, uint symbols_per_sec)
+        {
+            Console.Clear();
+            int forward = 0;
+            int heigth = 0;
+            string text = "Now NASA is working towards logging some of the smaller asteroids, those measuring 140 metres" +
+                          "wide or more. Of the 25,000 estimated asteroids of this size, so far about 8,000 have been logged" +
+                          "leaving 17,000 unaccounted for. Considering that a 19-metre asteroid that exploded above the city" +
+                          "of Chelyabinsk in Russia in 2013 injured 1,200 people, these middle-sized asteroids would be a   " +
+                          "serious danger if they enter Earth's orbit.";
+
+            Console.Write(text);
+            Thread start_timer = new Thread(Timer);
+            start_timer.Start();
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.SetCursorPosition(forward, heigth);
+                ConsoleKeyInfo sym = Console.ReadKey();
+                if (sym.KeyChar == text[i])
+                {
+                    Console.SetCursorPosition(forward, heigth);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(sym.KeyChar);
+                    forward++;
+                    symbols_per_sec++;
+                }
+                else 
+                {
+                    Console.SetCursorPosition(forward, heigth);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    forward++;
+                }
+                if (forward == 120)
+                {
+                    forward = 0;
+                    heigth++;
+                }
+            }
+        }
+        private static void Timer()
+        {
+            int remTime = 60;
+            Stopwatch st_Watch = new Stopwatch();
+            do
+            {
+                Console.SetCursorPosition(10, 10);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                if (remTime == 60)
+                {
+                    st_Watch.Start();
+                    Console.WriteLine("1:0");
+                    remTime--;
+                }
+                else
+                {
+                    Console.WriteLine($"0:{remTime--}");
+                    if (remTime < 10)
+                    {
+                        Console.SetCursorPosition(10, 10);
+                        Console.WriteLine($"0:0{remTime--}");
+                    }
+                    if (remTime == 0)
+                        st_Watch.Stop();
+                }
+                Thread.Sleep(1000);
+                Console.SetCursorPosition(10, 10);
+                Console.WriteLine("     ");
+            } while (remTime != 0);
+            Console.SetCursorPosition(10, 10);
+            Console.WriteLine($"0:{remTime}");
+
+        }
+    }
+}
---
+++ FAST_TYPER_8/FastTyper/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FastTyper;
+namespace MyNameSpace
+{
+    class MyProgram
+    {
+        static int Main()
+        {
+            Player MyPlayer = new Player();
+            Console.Write("Enter your name for records table: ");
+            string RealName = Console.ReadLine();
+            MyPlayer.Name = RealName;
+            MyPlayer.SymsPerMins = 0;
+            MyPlayer.SymsPerSecs = 0;
+            Enter.TextChallenge(MyPlayer.Name, MyPlayer.SymsPerMins, MyPlayer.SymsPerSecs);
+            return 0;
+        }
+    }
+}
